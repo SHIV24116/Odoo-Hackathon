95bb9f46f91afe37f2ea6af6c324ce824d88712f
Isha yadav <ishayadav@10.152.102.185>
2026-07-12 14:45:57 +0530

    Add dashboard components

---
+++ Frontend/src/components/dashboard/TripChart.jsx
@@ -0,0 +1,7 @@
+import Bars from '../common/Bars'
+
+function TripChart({ values }) {
+  return <Bars values={values} />
+}
+
+export default TripChart
---
+++ Frontend/src/components/dashboard/VehicleChart.jsx
@@ -0,0 +1,7 @@
+import Bars from '../common/Bars'
+
+function VehicleChart({ values }) {
+  return <Bars values={values} />
+}
+
+export default VehicleChart
